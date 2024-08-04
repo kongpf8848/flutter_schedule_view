1b1f688455591ada1a59fe47c5f7d80ca50ff399
kongpf <kongpf8848@gmail.com>
2024-08-04 22:21:39 +0800

    remove unused code

--- lib/flutter_schedule_view.dart
+++ lib/flutter_schedule_view.dart
@@ -1,19 +1,16 @@
 library flutter_schedule_view;
 
-export 'src/controller/day_view.dart';
-export 'src/controller/week_view.dart';
-export 'src/controller/zoom_controller.dart';
 export 'src/event.dart';
+export 'src/controller/day_view.dart';
+export 'src/controller/zoom_controller.dart';
 export 'src/styles/day_bar.dart';
 export 'src/styles/day_view.dart';
 export 'src/styles/drag_and_drop.dart';
 export 'src/styles/hours_column.dart';
 export 'src/styles/resize_event.dart';
-export 'src/styles/week_view.dart';
 export 'src/styles/zoomable_header_widget.dart';
 export 'src/utils/callback_helpers.dart';
 export 'src/utils/hour_minute.dart';
 export 'src/widgets/day_bar.dart';
 export 'src/widgets/day_view.dart';
-export 'src/widgets/week_view.dart';
 export 'src/widgets/zoomable_header_widget.dart';
