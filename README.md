5dccd05177e09f8e3263f6771387fb7cc3ca86ae
kongpf <kongpf8848@gmail.com>
2024-08-04 20:39:09 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,7 +1,160 @@
 # Flutter Schedule View
 
-## Screenshots
+## 截图
 
 | Drag & Resize | Light mode | Dark mode |
 | :-----: | :-----: | :-----: |
 |![https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/drag_and_resize.gif](https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/drag_and_resize.gif)|![https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/schedule_view_light.jpg](https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/schedule_view_light.jpg)|![https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/schedule_view_dark.jpg](https://github.com/kongpf8848/flutter_schedule_view/tree/master/screenshots/schedule_view_dark.jpg)|
+
+## 功能特点
+- 显示可自定义的时间表视图，用于管理任务和约会。
+- 允许用户在计划中拖放任务。
+- 允许用户调整条目大小以调整其持续时间
+- 支持亮色模式和暗色模式进行切换
+
+## 导入
+
+## 使用
+```dart
+  DayView(
+      //当前显示日期
+      date: DateTime.now(),
+      //开始时间
+      initialTime: const HourMinute(hour: 6),
+      //是否缩放
+      userZoomable: false,
+      //显示开始时间/结束时间
+      showTimeRangeText: true,
+      //事件列表
+      events: events,
+      //风格
+      style: DayViewStyle(
+          backgroundColor: Theme
+              .of(context)
+              .twColors
+              .primaryBackgroundColor,
+          hourRowHeight: 60,
+          headerSize: 0,
+          backgroundRulesColor:
+          Theme
+              .of(context)
+              .twColors
+              .dividerBackgroundColor,
+          currentTimeRuleColor: Theme
+              .of(context)
+              .twColors
+              .primary,
+          currentTimeCircleColor: Theme
+              .of(context)
+              .twColors
+              .primary,
+          currentTimeCircleRadius: 5,
+          currentTimeRuleHeight: 1,
+          currentTimeCirclePosition: CurrentTimeCirclePosition.left,
+          currentTimeTextStyle: TextStyle(
+              color: Theme
+                  .of(context)
+                  .twColors
+                  .primary,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              height: 1.5)),
+      //小时风格
+      hoursColumnStyle: HoursColumnStyle(
+        width: 55,
+        color: Theme
+            .of(context)
+            .twColors
+            .primaryBackgroundColor,
+        textAlignment: Alignment.center,
+        textStyle: TextStyle(
+            color: Theme
+                .of(context)
+                .twColors
+                .secondTextColor,
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            height: 1.5),
+        timeRangeTextStyle: TextStyle(
+            color: Theme
+                .of(context)
+                .twColors
+                .primary,
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            height: 1.5),
+        interval: const Duration(hours: 1),
+      ),
+      //拖放选项
+      dragAndDropOptions: DragAndDropOptions(
+        startingGesture: isMobilePlatform()
+            ? DragStartingGesture.longPress
+            : DragStartingGesture.tap,
+        onEventDragged: (FlutterWeekViewEvent event, DateTime newStartTime) {
+          DateTime roundedTime = roundTimeToFitGrid(newStartTime,
+              gridGranularity: const Duration(minutes: 15));
+          event.shiftEventTo(roundedTime);
+        },
+      ),
+      //改变大小选项
+      resizeEventOptions: ResizeEventOptions(
+        minimumEventDuration: const Duration(minutes: 30),
+        onEventResizedUp:
+            (FlutterWeekViewEvent event, DateTime newStartTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizedUp:start:${event
+                  .start},newStartTime:${newStartTime}');
+          event.start = newStartTime;
+        },
+        onEventResizeUpUpdate:
+            (FlutterWeekViewEvent event, DateTime newStartTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizeUpUpdate:start:${event
+                  .start},newStartTime:${newStartTime}');
+          event.start = newStartTime;
+        },
+        onEventResizedDown:
+            (FlutterWeekViewEvent event, DateTime newEndTime) {
+          debugPrint(
+              '+++++++++++++++onEventResized:end:${event
+                  .end},newEndTime:${newEndTime}');
+          event.end = newEndTime;
+        },
+        onEventResizeDownUpdate:
+            (FlutterWeekViewEvent event, DateTime newEndTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizeUpdate:end:${event
+                  .end},newEndTime:${newEndTime}');
+          event.end = newEndTime;
+        },
+      ),
+      //是否可以拖放
+      dragPredicate: (event) {
+        return event.isNewEvent;
+      },
+      //是否可以拖拽
+      resizePredicate: (event) {
+        return event.isNewEvent;
+      },
+      //点击事件
+      onBackgroundTappedDown: (DateTime dateTime) {
+        dateTime = roundTimeToFitGrid(dateTime,
+            gridGranularity: const Duration(minutes: 15));
+        setState(() {
+          events.removeWhere((element) => element.isNewEvent);
+          events.add(FlutterWeekViewEvent(
+              title: NEW_EVENT_TEXT,
+              description: NEW_EVENT_TEXT,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              decoration: () => newEventDecoration(context),
+              eventTextBuilder: createEventTextBuilder,
+              start: dateTime,
+              end: dateTime.add(const Duration(minutes: 30)),
+              isNewEvent: true,
+              onTap: (event) {
+                print('+++++++++++++++++++++FlutterWeekViewEvent onTap');
+              }));
+        });
+      },
+    );
+```
--- example/lib/pages/dynamic_day_view.dart
+++ example/lib/pages/dynamic_day_view.dart
@@ -51,113 +51,12 @@ class _DynamicDayViewState extends State<DynamicDayView> {
 
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
     if (events.isEmpty) {
       _initEvents();
     }
     return Scaffold(
-      appBar: _getAppBar(),
-      body: DayView(
-        date: now,
-        initialTime: const HourMinute(hour: 6),
-        userZoomable: false,
-        showTimeRangeText: true,
-        events: events,
-        style: DayViewStyle(
-            backgroundColor: Theme.of(context).twColors.primaryBackgroundColor,
-            hourRowHeight: 60,
-            headerSize: 0,
-            backgroundRulesColor:
-                Theme.of(context).twColors.dividerBackgroundColor,
-            currentTimeRuleColor: Theme.of(context).twColors.primary,
-            currentTimeCircleColor: Theme.of(context).twColors.primary,
-            currentTimeCircleRadius: 5,
-            currentTimeRuleHeight: 1,
-            currentTimeCirclePosition: CurrentTimeCirclePosition.left,
-            currentTimeTextStyle: TextStyle(
-                color: Theme.of(context).twColors.primary,
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                height: 1.5)),
-        hoursColumnStyle: HoursColumnStyle(
-          width: 55,
-          color: Theme.of(context).twColors.primaryBackgroundColor,
-          textAlignment: Alignment.center,
-          textStyle: TextStyle(
-              color: Theme.of(context).twColors.secondTextColor,
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              height: 1.5),
-          timeRangeTextStyle: TextStyle(
-              color: Theme.of(context).twColors.primary,
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              height: 1.5),
-          interval: const Duration(hours: 1),
-        ),
-        dragAndDropOptions: DragAndDropOptions(
-          startingGesture: isMobilePlatform()
-              ? DragStartingGesture.longPress
-              : DragStartingGesture.tap,
-          onEventDragged: (FlutterWeekViewEvent event, DateTime newStartTime) {
-            DateTime roundedTime = roundTimeToFitGrid(newStartTime,
-                gridGranularity: const Duration(minutes: 15));
-            event.shiftEventTo(roundedTime);
-          },
-        ),
-        resizeEventOptions: ResizeEventOptions(
-          minimumEventDuration: const Duration(minutes: 30),
-          onEventResizedUp:
-              (FlutterWeekViewEvent event, DateTime newStartTime) {
-            debugPrint(
-                '+++++++++++++++onEventResizedUp:start:${event.start},newStartTime:${newStartTime}');
-            event.start = newStartTime;
-          },
-          onEventResizeUpUpdate:
-              (FlutterWeekViewEvent event, DateTime newStartTime) {
-            debugPrint(
-                '+++++++++++++++onEventResizeUpUpdate:start:${event.start},newStartTime:${newStartTime}');
-            event.start = newStartTime;
-          },
-          onEventResizedDown:
-              (FlutterWeekViewEvent event, DateTime newEndTime) {
-            debugPrint(
-                '+++++++++++++++onEventResized:end:${event.end},newEndTime:${newEndTime}');
-            event.end = newEndTime;
-          },
-          onEventResizeDownUpdate:
-              (FlutterWeekViewEvent event, DateTime newEndTime) {
-            debugPrint(
-                '+++++++++++++++onEventResizeUpdate:end:${event.end},newEndTime:${newEndTime}');
-            event.end = newEndTime;
-          },
-        ),
-        dragPredicate: (event) {
-          return event.isNewEvent;
-        },
-        resizePredicate: (event) {
-          return event.isNewEvent;
-        },
-        onBackgroundTappedDown: (DateTime dateTime) {
-          dateTime = roundTimeToFitGrid(dateTime,
-              gridGranularity: const Duration(minutes: 15));
-          setState(() {
-            events.removeWhere((element) => element.isNewEvent);
-            events.add(FlutterWeekViewEvent(
-                title: NEW_EVENT_TEXT,
-                description: NEW_EVENT_TEXT,
-                padding: EdgeInsets.only(left: 16, right: 16),
-                decoration: () => newEventDecoration(context),
-                eventTextBuilder: createEventTextBuilder,
-                start: dateTime,
-                end: dateTime.add(const Duration(minutes: 30)),
-                isNewEvent: true,
-                onTap: (event) {
-                  print('+++++++++++++++++++++FlutterWeekViewEvent onTap');
-                }));
-          });
-        },
-      ),
+        appBar: _getAppBar(),
+        body: _getScheduleView()
     );
   }
 
@@ -173,7 +72,10 @@ class _DynamicDayViewState extends State<DynamicDayView> {
             "9楼光明顶会议室",
             textAlign: TextAlign.center,
             style: TextStyle(
-              color: Theme.of(context).twColors.primaryTextColor,
+              color: Theme
+                  .of(context)
+                  .twColors
+                  .primaryTextColor,
               fontSize: 16,
               fontWeight: FontWeight.bold,
             ),
@@ -182,13 +84,160 @@ class _DynamicDayViewState extends State<DynamicDayView> {
             "${DateFormat('M月d日(E)').format(DateTime.now())}",
             textAlign: TextAlign.center,
             style: TextStyle(
-              color: Theme.of(context).twColors.primaryTextColor,
+              color: Theme
+                  .of(context)
+                  .twColors
+                  .primaryTextColor,
               fontSize: 14,
               fontWeight: FontWeight.w500,
             ),
           )
         ],
       ),
+    );
+  }
+
+  Widget _getScheduleView() {
+    return DayView(
+      //当前显示日期
+      date: DateTime.now(),
+      //开始时间
+      initialTime: const HourMinute(hour: 6),
+      //是否缩放
+      userZoomable: false,
+      //显示开始时间/结束时间
+      showTimeRangeText: true,
+      //事件列表
+      events: events,
+      //风格
+      style: DayViewStyle(
+          backgroundColor: Theme
+              .of(context)
+              .twColors
+              .primaryBackgroundColor,
+          hourRowHeight: 60,
+          headerSize: 0,
+          backgroundRulesColor:
+          Theme
+              .of(context)
+              .twColors
+              .dividerBackgroundColor,
+          currentTimeRuleColor: Theme
+              .of(context)
+              .twColors
+              .primary,
+          currentTimeCircleColor: Theme
+              .of(context)
+              .twColors
+              .primary,
+          currentTimeCircleRadius: 5,
+          currentTimeRuleHeight: 1,
+          currentTimeCirclePosition: CurrentTimeCirclePosition.left,
+          currentTimeTextStyle: TextStyle(
+              color: Theme
+                  .of(context)
+                  .twColors
+                  .primary,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              height: 1.5)),
+      //小时风格
+      hoursColumnStyle: HoursColumnStyle(
+        width: 55,
+        color: Theme
+            .of(context)
+            .twColors
+            .primaryBackgroundColor,
+        textAlignment: Alignment.center,
+        textStyle: TextStyle(
+            color: Theme
+                .of(context)
+                .twColors
+                .secondTextColor,
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            height: 1.5),
+        timeRangeTextStyle: TextStyle(
+            color: Theme
+                .of(context)
+                .twColors
+                .primary,
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            height: 1.5),
+        interval: const Duration(hours: 1),
+      ),
+      //拖放选项
+      dragAndDropOptions: DragAndDropOptions(
+        startingGesture: isMobilePlatform()
+            ? DragStartingGesture.longPress
+            : DragStartingGesture.tap,
+        onEventDragged: (FlutterWeekViewEvent event, DateTime newStartTime) {
+          DateTime roundedTime = roundTimeToFitGrid(newStartTime,
+              gridGranularity: const Duration(minutes: 15));
+          event.shiftEventTo(roundedTime);
+        },
+      ),
+      //改变大小选项
+      resizeEventOptions: ResizeEventOptions(
+        minimumEventDuration: const Duration(minutes: 30),
+        onEventResizedUp:
+            (FlutterWeekViewEvent event, DateTime newStartTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizedUp:start:${event
+                  .start},newStartTime:${newStartTime}');
+          event.start = newStartTime;
+        },
+        onEventResizeUpUpdate:
+            (FlutterWeekViewEvent event, DateTime newStartTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizeUpUpdate:start:${event
+                  .start},newStartTime:${newStartTime}');
+          event.start = newStartTime;
+        },
+        onEventResizedDown:
+            (FlutterWeekViewEvent event, DateTime newEndTime) {
+          debugPrint(
+              '+++++++++++++++onEventResized:end:${event
+                  .end},newEndTime:${newEndTime}');
+          event.end = newEndTime;
+        },
+        onEventResizeDownUpdate:
+            (FlutterWeekViewEvent event, DateTime newEndTime) {
+          debugPrint(
+              '+++++++++++++++onEventResizeUpdate:end:${event
+                  .end},newEndTime:${newEndTime}');
+          event.end = newEndTime;
+        },
+      ),
+      //是否可以拖放
+      dragPredicate: (event) {
+        return event.isNewEvent;
+      },
+      //是否可以拖拽
+      resizePredicate: (event) {
+        return event.isNewEvent;
+      },
+      //点击事件
+      onBackgroundTappedDown: (DateTime dateTime) {
+        dateTime = roundTimeToFitGrid(dateTime,
+            gridGranularity: const Duration(minutes: 15));
+        setState(() {
+          events.removeWhere((element) => element.isNewEvent);
+          events.add(FlutterWeekViewEvent(
+              title: NEW_EVENT_TEXT,
+              description: NEW_EVENT_TEXT,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              decoration: () => newEventDecoration(context),
+              eventTextBuilder: createEventTextBuilder,
+              start: dateTime,
+              end: dateTime.add(const Duration(minutes: 30)),
+              isNewEvent: true,
+              onTap: (event) {
+                print('+++++++++++++++++++++FlutterWeekViewEvent onTap');
+              }));
+        });
+      },
     );
   }
 }
