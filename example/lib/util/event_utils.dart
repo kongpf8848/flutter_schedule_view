8950529c6370b9732e550833427b6698d401e55a
kongpf <kongpf8848@gmail.com>
2024-08-06 14:25:10 +0800

    update code

--- example/lib/util/event_utils.dart
+++ example/lib/util/event_utils.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_schedule_view/flutter_schedule_view.dart';
 import 'package:flutter_schedule_view_example/theme/app_theme.dart';
